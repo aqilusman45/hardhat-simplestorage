91a818ec10eac2010e4adebbc07f1c4a2efd8788
aqilusman45 <aqilusman45@gmail.com>
2022-08-06 18:09:54 -0500

    Hardhat Fundamentals - add new networks - new network params are   - url   - accounts -> list of private keys   - chainId

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config"
+import dotenv from "dotenv"
+import "@nomicfoundation/hardhat-toolbox"
+
+dotenv.config()
+
+const { GANACHE_RPC_URL, GANACH_ACCOUNT_PRIVATE_KEY = "" } = process.env
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
-};
+  defaultNetwork: "hardhat",
+  networks: {
+    ganache: {
+      url: GANACHE_RPC_URL,
+      accounts: [GANACH_ACCOUNT_PRIVATE_KEY],
+      chainId: 1337,
+    },
+  },
+}
 
-export default config;
+export default config
