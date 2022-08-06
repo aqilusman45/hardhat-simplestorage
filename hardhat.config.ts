3d90d9a1f248c8cd65a1e7815f10af42d2db79fb
aqilusman45 <aqilusman45@gmail.com>
2022-08-06 18:52:04 -0500

    HardHat Fundamentals - import and configure plugins.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config"
 import dotenv from "dotenv"
 import "@nomicfoundation/hardhat-toolbox"
+import "@nomiclabs/hardhat-etherscan"
 
 dotenv.config()
 
-const { GANACHE_RPC_URL, GANACH_ACCOUNT_PRIVATE_KEY = "" } = process.env
+export const {
+  GANACHE_RPC_URL,
+  GANACH_ACCOUNT_PRIVATE_KEY = "",
+  ETHERSCAN_API_KEY,
+} = process.env
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   defaultNetwork: "hardhat",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
   networks: {
     ganache: {
       url: GANACHE_RPC_URL,
